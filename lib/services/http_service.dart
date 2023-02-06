189fb83db71b485e33c39295fdd6aa1c7d269f7a
nghiadt <nghiadt@arrowhitech.co>
2023-02-06 18:05:42 +0700

    update fetch api

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -13,3 +13,27 @@ Future<dynamic> fetchData(String url) async {
     throw Exception('Failed to load data');
   }
 }
+Future<dynamic> post(String url, body) async {
+  try {
+    final response = await http.post(Uri.parse(url),body: body);
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load data');
+    }
+  } catch (e) {
+    throw Exception('Failed to load data');
+  }
+}
+Future<dynamic> put(String url, body) async {
+  try {
+    final response = await http.put(Uri.parse(url),body:body);
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load data');
+    }
+  } catch (e) {
+    throw Exception('Failed to load data');
+  }
+}
